116978e97399aa9382ed054b65adb935f5b75247
amsyarasyiq <82711525+amsyarasyiq@users.noreply.github.com>
2023-03-16 17:36:27 +0800

    reduce file size by half

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 // Based off https://github.com/amsyarasyiq/aliucordrn-plugins/blob/8b924a5bd20e4ed345b9a2c998660ba3669d2a8b/MagicThemeFixer/index.tsx
 import { Plugin, registerPlugin } from "enmity/managers/plugins";
-import { Dispatcher, Flux } from "enmity/metro/common";
 import { getByProps, getModule } from "enmity/metro";
 import manifest from "../manifest.json";
 
-const FluxDispatcher = Dispatcher as typeof Flux.Dispatcher;
+// @ts-expect-error
+const FluxDispatcher = window.enmity.modules.common.Dispatcher as any;
 
 const ThemeStore = getModule((m) => m?.getName?.() === "ThemeStore");
 const UnsyncedUserSettingsStore = getModule((m) => m?.getName?.() === "UnsyncedUserSettingsStore");
